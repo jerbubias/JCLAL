5d6646dcf4654693d9b78750f783dca437b8120a
tgcunha999 <33125227+tgcunha999@users.noreply.github.com>
2019-10-30 19:47:40 +0000

    unknown pt 2

--- src/main/java/net/sf/jclal/activelearning/singlelabel/querystrategy/WithUnknownClassQueryStrategy.java
+++ src/main/java/net/sf/jclal/activelearning/singlelabel/querystrategy/WithUnknownClassQueryStrategy.java
@@ -14,16 +14,44 @@ public class WithUnknownClassQueryStrategy extends AbstractSingleLabelQueryStrat
         EuclideanDistance euc = new EuclideanDistance(getLabelledData().getDataset());
 
         double max = 0;
-        double temp;
+        double temp,sum=0;
 
-        for (Instance current : getLabelledData().getDataset()) {
+        /*for (Instance current : getLabelledData().getDataset()) {
             temp = 1 / (1 + euc.distance(instance, current));
             if(temp > max)
                 max = temp;
         }
+        */
+
+        for (Instance current : getLabelledData().getDataset()) {
+            temp = 1 / (1 + euc.distance(instance, current));
+            if (temp > max)
+                max = temp;
+            sum += temp;
+        }
 
         double simUnknown = 1 - max;
 
+        double prob = simUnknown / sum;
+
+        /*
+        double sumatoria = 0;
+        double log;
+
+        for (double current : probabilities) {
+
+            if (current != 0) {
+                log = logbase2(current);
+                sumatoria += current * log;
+            }
+        }
+
+        return (sumatoria != 0) ? (-sumatoria) : 0;
+        */
         return 0;
+    }
+
+    private double logbase2(double d) {
+        return Math.log(d) / Math.log(2);
     }
 }
