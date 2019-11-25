df220eff2f185a94dc99bef7c172f2c88605bf4f
jerbubias <samuelbouteiro@gmail.com>
2019-11-25 13:43:45 +0000

    Update SurpriseBased.java

--- src/main/java/net/sf/jclal/activelearning/stopcriteria/SurpriseBased.java
+++ src/main/java/net/sf/jclal/activelearning/stopcriteria/SurpriseBased.java
@@ -5,6 +5,12 @@ import net.sf.jclal.core.IAlgorithm;
 import net.sf.jclal.core.IConfigure;
 import net.sf.jclal.core.IStopCriterion;
 import org.apache.commons.configuration.Configuration;
+import weka.clusterers.SimpleKMeans;
+import weka.core.EuclideanDistance;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.Remove;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -29,14 +35,43 @@ public class SurpriseBased implements IStopCriterion, IConfigure {
     public boolean stop(IAlgorithm algorithm) {
         int i;
         double maxProb, surprise, maxValue;
-        ArrayList<String> lastInstances;
+        ArrayList<String> queriedInstances;
+        int numQueriedInstances, numLabelledInstances;
+        Instances labelledInstances;
+        Instances oldInstances;
+        Instances lastInstances;
         double[] prob;
-        // Get Instances Queried To The Oracle
-        lastInstances = ((AbstractOracle) algorithm.getScenario().getOracle()).getLastLabeledInstances();
-        if (lastInstances != null) {
-            for (i = 0; i < lastInstances.size(); i++) {
+        // Get Instances Queried To The Oracle In String Format
+        queriedInstances = ((AbstractOracle) algorithm.getScenario().getOracle()).getLastLabeledInstances();
+        if (queriedInstances != null) {
+            // Get Number Of Instances Queried To The Oracle
+            numQueriedInstances = queriedInstances.size();
+            // Get Instances Labelled By The Oracle
+            labelledInstances = algorithm.getLabeledDataSet().getDataset();
+            // Get Number Of Instances Labelled By The Oracle
+            numLabelledInstances = labelledInstances.numInstances();
+            // Get Instances Previously Labelled By The Oracle
+            oldInstances = new Instances(labelledInstances, 0, numLabelledInstances - numQueriedInstances);
+            // Get Last Instances Labelled By The Oracle
+            lastInstances = new Instances(labelledInstances, numLabelledInstances - numQueriedInstances, numQueriedInstances);
+            /*
+            // Print Instances Labelled By The Oracle
+            System.out.println(numLabelledInstances);
+            System.out.println(labelledInstances);
+            // Print Instances Previously Labelled By The Oracle
+            System.out.println(numLabelledInstances - numQueriedInstances);
+            System.out.println(oldInstances);
+            // Print Last Instances Labelled By The Oracle
+            System.out.println(numQueriedInstances);
+            System.out.println(lastInstances);
+             */
+            for (i = 0; i < numQueriedInstances; i++) {
                 // Get Probability Distribution
-                prob = new double[]{0.25, 0.25, 0.25, 0.25};
+                prob = getProbabilityDistribution(oldInstances, lastInstances.get(i));
+                /*
+                // Print Probability Distribution
+                System.out.println(Arrays.toString(prob));
+                 */
                 maxProb = maxArray(prob);
                 // Calculate Surprise
                 surprise = 1 - maxProb;
@@ -45,7 +80,7 @@ public class SurpriseBased implements IStopCriterion, IConfigure {
                 // Get Maximum Value In The Interval Window
                 maxValue = maxArray(this.inrValues);
                 // Stop Training If Certain Criterion Are Valid (Less Or Equal Than The Maximum Surprise)
-                if (maxValue > this.maxSurprise)
+                if (maxValue < this.maxSurprise)
                     return true;
             }
         }
@@ -90,10 +125,111 @@ public class SurpriseBased implements IStopCriterion, IConfigure {
     private static double maxArray(double[] array) {
         int i;
         double max = 0.0;
-        for(i = 0; i < array.length; i++) {
+        for(i = 0; i < array.length-1; i++) {
             if(array[i] > max)
                 max = array[i];
         }
         return max;
+    }
+
+    private double[] getProbabilityDistribution(Instances labelledInstances, Instance instance) {
+        int i;
+        double sim, sumSim, maxSim, unkSim;
+        Instances centroids;
+        Instance formattedInstance;
+        EuclideanDistance euc;
+        double[] prob;
+        sumSim = 0;
+        maxSim = 0;
+        // Centroids Calculation
+        centroids = getCentroids(labelledInstances);
+        // Similarities Calculation
+        formattedInstance = getClassless(instance, labelledInstances);
+        euc = new EuclideanDistance(centroids);
+        euc.setDontNormalize(true);
+        prob = new double[centroids.numInstances() + 1];
+        for (i = 0; i < centroids.numInstances(); i++) {
+            sim = 1 / (1 + euc.distance(formattedInstance, centroids.get(i)));
+            prob[i] = sim;
+            sumSim += sim;
+            if (sim > maxSim)
+                maxSim = sim;
+        }
+        // Similarity of Unknown Calculation
+        unkSim = 1 - maxSim;
+        prob[i] = unkSim;
+        sumSim += unkSim;
+        // Calculate Probability Distribution
+        for (i = 0; i < prob.length; i++) {
+            prob[i] = prob[i] / sumSim;
+        }
+        // Return Probability Distribution
+        return prob;
+    }
+
+    private Instances getCentroids(Instances instances) {
+        int i, j, ni;
+        Instances centroids = null;
+        int numClasses = instances.numClasses();
+        int numInstances =  instances.numInstances();
+        for(i = 0; i < numClasses; i++) {
+            Instances classInstances = new Instances(instances, 0);
+            for(j = 0; j < numInstances; j++) {
+                if((int)instances.get(j).classValue() == i) {
+                    classInstances.add(instances.get(j));
+                }
+            }
+            ni = classInstances.numInstances();
+            if(ni > 0) {
+                classInstances = getClassless(classInstances);
+                if(centroids == null) {
+                    centroids = new Instances(classInstances, 0);
+                }
+                if(ni == 1) {
+                    centroids.add(classInstances.get(0));
+                }
+                else {
+                    SimpleKMeans kMeans = new SimpleKMeans();
+                    try {
+                        kMeans.setNumClusters(1);
+                        kMeans.buildClusterer(classInstances);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        continue;
+                    }
+                    classInstances = kMeans.getClusterCentroids();
+                    centroids.add(classInstances.get(0));
+                }
+            }
+        }
+        if(centroids == null) {
+            centroids = new Instances(instances, 0);
+        }
+        return centroids;
+    }
+
+    private Instance getClassless(Instance instance, Instances instances) {
+        Remove filter = new Remove();
+        filter.setAttributeIndices("" + (instances.classIndex() + 1));
+        try {
+            filter.setInputFormat(instances);
+            filter.input(instance);
+            return filter.output();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return instance;
+        }
+    }
+
+    private Instances getClassless(Instances instances) {
+        Remove filter = new Remove();
+        filter.setAttributeIndices("" + (instances.classIndex() + 1));
+        try {
+            filter.setInputFormat(instances);
+            return Filter.useFilter(instances, filter);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return instances;
+        }
     }
 }
